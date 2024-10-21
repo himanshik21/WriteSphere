814ba146033877f8f07d08f3aed94d6b6f41f687
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2024-10-21 18:32:05 +0530

    Start a Mern Stack Blog Web App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,77 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2020/01/GettyImages-1096026706.jpg?resize=708,497"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>
+            Groww pays $160M tax as it returns to India amid a startup
+            relocation wave
+          </h2>
+          <p>
+            While the IPO market remains subdued in the U.S. and many developed
+            markets, India has emerged as a hotbed for public offerings this
+            year.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2020/01/GettyImages-1096026706.jpg?resize=708,497"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>
+            Groww pays $160M tax as it returns to India amid a startup
+            relocation wave
+          </h2>
+          <p>
+            While the IPO market remains subdued in the U.S. and many developed
+            markets, India has emerged as a hotbed for public offerings this
+            year.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2020/01/GettyImages-1096026706.jpg?resize=708,497"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>
+            Groww pays $160M tax as it returns to India amid a startup
+            relocation wave
+          </h2>
+          <p>
+            While the IPO market remains subdued in the U.S. and many developed
+            markets, India has emerged as a hotbed for public offerings this
+            year.{" "}
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
